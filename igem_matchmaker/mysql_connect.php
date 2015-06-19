27dfb5539715614083605aade6f0a305c2300638
Youssef Chahibi <chahibi@gmail.com>
2015-06-19 16:39:23 +0200

    Git ignore file

--- igem_matchmaker/mysql_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$con = mysql_connect("localhost","igem_matchmaker","iGEM2015!");
-	if (!$con)
-	  {
-	   die('Could not connect: ' . mysql_error());
-	  }
-	mysql_select_db("igem_db", $con);
-?>
